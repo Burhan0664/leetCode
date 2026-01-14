5647c3be5660e89ac8fac5997cf440eabe8c4d07
Burhan0664 <Burhan0664@hotmail.com>
2026-01-14 16:39:30 +0300

    new algorithms solved

---
+++ Algorithms/C#/AddBinary/AddBinary/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public string AddBinary(string a, string b)
+    {
+        int j = a.Length - 1;
+        int k = b.Length - 1;
+        int carry = 0;
+        string result = "";
+       
+
+        while(j >= 0 || k >= 0 || carry == 1) 
+        {
+            int sum = carry;
+            if(j >= 0)
+            {
+                sum += a[j--] - '0';           
+            }
+
+            if (k >= 0)
+            {
+                sum += b[k--] - '0';
+            }
+
+            result = (sum % 2) + result;
+            carry = sum / 2;
+        }
+        
+        return result;
+    }
+}
---
+++ Algorithms/C#/RomanToInteger/RomanToInteger/Program.cs
@@ -0,0 +1,69 @@
+﻿public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        int result = 0;
+        char x,y;
+        int negative = 0;
+        var liste = new List<(char Key, int Value)>
+        {
+            ('I',1),
+            ('V',5),
+            ('X',10),
+            ('L',50),
+            ('C',100),
+            ('D',500),
+            ('M',1000)
+        };
+
+        foreach (var c in s)
+        {
+            foreach (var e in liste)
+            {
+                if (e.Key == c)
+                {
+                    result += e.Value;
+                    break;
+                }
+                
+            }
+        }
+        for (int a = 0; a < s.Length; a++)
+        {
+            if (s[a] == 'I')
+            {
+                for (int i = a; i < a+2; i++)
+                {
+                    if (s[i] == 'V' || s[i] == 'X')
+                    {
+                        negative += 2;
+                    }
+                }
+            }
+            if (s[a] == 'X')
+            {
+                for (int i = a; i < a + 2; i++)
+                {
+                    if (s[i] == 'L' || s[i] == 'C')
+                    {
+                        negative += 20;
+                    }
+                }
+            }
+            if (s[a] == 'C')
+            {
+                for (int i = a; i < a + 2; i++)
+                {
+                    if (s[i] == 'D' || s[i] == 'M')
+                    {
+                        negative += 200;
+                    }
+                }
+            }
+        }
+ 
+
+
+        return result-negative;
+    }
+}
