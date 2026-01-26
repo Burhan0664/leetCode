7ff6919a36f112e793f3b786a5306274c2bb58ac
Burhan0664 <Burhan0664@hotmail.com>
2026-01-26 23:17:52 +0300

    new algorithm problems solved

---
+++ Algorithms/C#/İsPalindrome/İsPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        for (int i = 0,j=s.Length-1; j > i;)
+        {
+            if (!char.IsLetterOrDigit(s[i]))
+            {
+                i++;
+                continue;
+            }
+
+            if (!char.IsLetterOrDigit(s[j]))
+            {
+                j--;
+                continue;
+            }
+
+            if (char.ToLower(s[j--]) != char.ToLower(s[i++]))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
