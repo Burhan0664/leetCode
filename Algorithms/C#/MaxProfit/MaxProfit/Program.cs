6eb41547cd7f19b755d30375b0b651a72417541c
Burhan0664 <Burhan0664@hotmail.com>
2026-02-01 14:49:10 +0300

    maxprofit

---
+++ Algorithms/C#/MaxProfit/MaxProfit/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (var price in prices)
+        {
+            if (price < minPrice)
+                minPrice = price;
+
+            int profit = price - minPrice;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+
+        return maxProfit;
+    }
+}
