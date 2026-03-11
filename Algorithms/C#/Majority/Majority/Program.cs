34330f0eea9b48855739a978d924fb2a3bc7706d
Burhan0664 <Burhan0664@hotmail.com>
2026-03-11 23:26:43 +0300

    Majority algoritm solved

---
+++ Algorithms/C#/Majority/Majority/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int indicator = nums.Length / 2;
+        int a = 0;
+        int result = 0;
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            foreach (int x in nums)
+            {
+                if (x == nums[i])
+                {
+                    a++;
+                }
+            }
+            if (indicator<a) 
+            {
+                result = nums[i];
+                break;
+            }
+            a = 0;
+        }
+        return result;
+    }
+}
