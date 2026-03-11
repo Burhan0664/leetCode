bd0ea8689ad0c0d05f70fccb89a164bd635fa177
Burhan0664 <Burhan0664@hotmail.com>
2026-03-12 01:47:27 +0300

    PowerOfTwo algotihm problem solved

---
+++ Algorithms/C#/PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        int result=n;
+        while (result >= 2)
+        {
+            if (result % 2 == 0)
+            {
+                result = result / 2;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+        return result == 1 ? true : false;
+    }
+}
