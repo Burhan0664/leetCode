8e31e22b709bab026de58926b8ef5c0d409fc703
Burhan0664 <Burhan0664@hotmail.com>
2026-03-12 16:06:41 +0300

    AddDigits algorithm problem solved

---
+++ Algorithms/C#/AddDigits/AddDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int AddDigits(int num)
+    {
+        string result = num.ToString();
+        int sum = 0;
+        while (result.Length > 1) 
+        {
+            sum = 0;
+            foreach (char c in result) 
+            {
+                sum += c - '0';
+            }
+            result = sum.ToString();
+        }
+        return Convert.ToInt32(result);
+    }
+}
