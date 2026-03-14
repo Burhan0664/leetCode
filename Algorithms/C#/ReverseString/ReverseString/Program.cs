c351fcabedd0972192e4af8e5747d6e69ab8b616
Burhan0664 <Burhan0664@hotmail.com>
2026-03-14 12:15:00 +0300

    ReverseString algotihm problem solved

---
+++ Algorithms/C#/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int lengthOfLast = s.Length - 1;
+        char hold;
+        for (int i = 0; i < s.Length/2; i++)
+        {
+            hold = s[lengthOfLast];
+            s[lengthOfLast] = s[i];
+            s[i] = hold;
+            lengthOfLast--;
+        }
+        Console.WriteLine(s);
+    }
+
+        public void ReverseString2(char[] s)
+        {
+
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+
+                i++;
+                j--;
+            }
+        }
+    }
