86327b193ab6ab6b8ace58331487b267c68aaa67
Burhan0664 <Burhan0664@hotmail.com>
2026-03-16 11:39:50 +0300

    ContainsDuplicate algorithm solved

---
+++ Algorithms/C#/ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (set.Contains(num))
+                return true;
+
+            set.Add(num);
+        }
+
+        return false;
+    }
+
+}
