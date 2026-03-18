bf5bb0ddebdd343b447ed1716a0502008866346d
Burhan0664 <Burhan0664@hotmail.com>
2026-03-18 18:32:10 +0300

    NumberOfOneBits algirithm problem solved

---
+++ Algorithms/C#/NumberOfOneBits/NumberOfOneBits/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int HammingWeight(int n)
+    {
+        string number = Convert.ToString(n,2);
+        int result = 0;
+        foreach(int i in number)
+        {
+            if(i == '1') 
+            {
+               result++;
+            }
+            
+        }
+        return result;
+    }
+}
