5742ef9d501a71f918ffe7d42670f5a664e99c99
Burhan0664 <Burhan0664@hotmail.com>
2026-04-05 19:39:57 +0300

    new algorithm of data structure

---
+++ dataStructure/MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            nums1[i]
+        }
+    }
+}
