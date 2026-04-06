75ad4027bb19362ba3d7a4311dd259b50fad6e51
Burhan0664 <Burhan0664@hotmail.com>
2026-04-06 15:02:39 +0300

    SymmetricTree algorithm solved

---
+++ dataStructure/SymmetricTree/SymmetricTree/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+        return IsSame(root.left, root.right);
+    }
+
+    public bool IsSame(TreeNode leftroot, TreeNode rightroot)
+    {
+        if (leftroot == null && rightroot == null)
+            return true;
+
+        if (leftroot == null || rightroot == null)
+            return false;
+
+        if (leftroot.val != rightroot.val)
+            return false;
+
+        return IsSame(leftroot.left, rightroot.right) &&
+               IsSame(leftroot.right, rightroot.left);
+    }
+}
