74da5fc00f679503e67b9d5decb0da86b58b4688
Burhan0664 <Burhan0664@hotmail.com>
2026-04-24 17:02:38 +0300

    MergeSortedArray algoritm problem solved

--- dataStructure/MergeSortedArray/MergeSortedArray/Program.cs
+++ dataStructure/MergeSortedArray/MergeSortedArray/Program.cs
@@ -2,9 +2,22 @@
 {
     public void Merge(int[] nums1, int m, int[] nums2, int n)
     {
-        for (int i = 0; i < nums1.Length; i++)
+        int last = m + n - 1;
+        int mind = m - 1;
+        int nind = n - 1;
+
+        while (nind>=0)
         {
-            nums1[i]
+            if (mind>=0 && nums1[mind] > nums2[nind])
+            {
+                nums1[last--] = nums1[mind--];
+            }
+            else
+            {
+                nums1[last--] = nums2[nind--];
+            }
         }
+
+
     }
 }
